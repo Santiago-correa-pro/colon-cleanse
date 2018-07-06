d3195a80129d88ee9eba504399ecfb713d599ede
Santiago Correa <santiago_correa@Santiagos-MacBook-Pro.local>
2018-07-06 12:33:15 -0500

    Added admin area and admin db

--- config/database.php
+++ config/database.php
@@ -1,9 +1,15 @@
 <?php 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
     $host='localhost';
     $database = 'colon';
     $user = 'santiago';
     $password = 'Rddrl101';
 
 
-    $conn = new PDO('mysql:host='.$host.';dbname='.$database,$user,$password);
+    $conn = new PDO('mysql:host='.$server.';dbname='.$db,$username,$password);
 ?>
+
